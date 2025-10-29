70507ea68ec19b331e6ecad1806a67db17f4df73
6b6169736569 <229914203+beach3developer@users.noreply.github.com>
2025-10-30 01:31:35 +0900

    fix sitemap and update astro.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://beach3developer.github.io',
+  site: 'https://beach3developer.github.io/',
   base: '/',
   integrations: [partytown({
     config: {
